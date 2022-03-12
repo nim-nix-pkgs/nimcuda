32fa77ae9f909a1bc8de33ed25525df002f483d9
hugosenari <hugosenari@gmail.com>
2022-03-12 04:26:33 -0300

    chore: re index nimcuda

---
+++ 0_1_6/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''CUDA bindings'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-nimcuda-0_1_6.flake = false;
+  inputs.src-nimcuda-0_1_6.owner = "andreaferretti";
+  inputs.src-nimcuda-0_1_6.ref   = "refs/tags/0.1.6";
+  inputs.src-nimcuda-0_1_6.repo  = "nimcuda";
+  inputs.src-nimcuda-0_1_6.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nimcuda-0_1_6"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-nimcuda-0_1_6";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
